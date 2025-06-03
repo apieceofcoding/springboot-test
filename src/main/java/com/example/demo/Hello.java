8b08e105cf4b214f630e9cb6eb2f5c08e4d8e1b1
sanserif28 <sanserif28@gmail.com>
2025-06-03 23:13:27 +0900

    feat: Hello

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Hello {
+
+    private final String name;
+
+    public Hello(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
