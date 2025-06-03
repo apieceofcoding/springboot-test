3aa390ba77678119e91a4a1a6fc5ca498c5d5d6d
sanserif28 <sanserif28@gmail.com>
2025-06-03 23:35:57 +0900

    lombok used

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,14 +1,13 @@
 package com.example.demo;
 
+import lombok.Getter;
+
+@Getter
 public class Hello {
 
     private final String name;
 
     public Hello(String name) {
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 }
